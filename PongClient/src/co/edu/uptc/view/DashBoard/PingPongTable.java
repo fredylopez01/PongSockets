660d1bdf259c93ffe9e9924dc064405188ac9e2b
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-05-16 08:23:02 -0500

    Arreglo la parte gráfica

--- PongClient/src/co/edu/uptc/view/DashBoard/PingPongTable.java
+++ PongClient/src/co/edu/uptc/view/DashBoard/PingPongTable.java
@@ -8,14 +8,18 @@ public class PingPongTable extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.BLUE);
-        g2d.fillRect(0, 0, getWidth(), getHeight());
-
         
-        g2d.setColor(Color.WHITE);
-        g2d.setStroke(new BasicStroke(8)); 
-        g2d.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
+        g.setColor(new Color(0x39a5f0));
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.setColor(new Color(0x15368b));
+        g.fillRect(0, 30, getWidth(), getHeight()-60);
+        
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 30, this.getWidth(), 10);
+        g.fillRect(0, this.getHeight()/2, this.getWidth(), 5);
+
+        g.fillRect(0, this.getHeight()-40, this.getWidth(), 10);
     }
    
 }
