ca169bfd39c90001f45edb1da52942fee29a6825
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-05-18 15:37:15 -0500

    Arreglos visuales

---
+++ PongClient/src/co/edu/uptc/view/DashBoard/LoadPanel.java
@@ -0,0 +1,35 @@
+package co.edu.uptc.view.DashBoard;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Image;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class LoadPanel extends JPanel {
+    private JLabel lbl;
+    private Image background;
+
+    public LoadPanel(){
+        initComponents();
+    }
+
+    private void initComponents() {
+        lbl = new JLabel("Cargando...");
+        lbl.setFont(new Font("Currier", Font.BOLD, 20));
+        this.setLayout(new BorderLayout());
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        bottomPanel.add(lbl);
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    public void paint(Graphics g) {
+        background = new ImageIcon(getClass().getResource("/co/edu/uptc/view/images/load.gif")).getImage();
+        g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), this);
+        setOpaque(false);
+        super.paint(g);
+    }
+}
